ca0c6a9071176d0b4b0b85f9c9b65d6a76d18527
Jakkima1 <70976980+Jakkima1@users.noreply.github.com>
2020-09-08 17:50:33 -0400

    Update train_RNN.py

--- train_RNN.py
+++ train_RNN.py
@@ -16,79 +16,79 @@ def main():
     from keras.layers import LSTM, Dropout, Dense, Activation
 
     # Loading the dataset
-    # train_data = pd.read_csv(r'./data/q2_dataset.csv')
+    train_data = pd.read_csv(r'./data/q2_dataset.csv')
     #
     # # Selecting volume, open, High aand low columns
-    # train_set = train_data.iloc[:, 2:6]
+    train_set = train_data.iloc[:, 2:6]
     # # Storing the dates in dates_data from the dataset
-    # dates_data = train_data.iloc[:, 0:1]
+    dates_data = train_data.iloc[:, 0:1]
     #
     # # selecting volume column and reshaping
-    # data1 = train_data[train_data.columns[2]]
-    # data1 = np.array(data1).reshape(-1, 1)
+    data1 = train_data[train_data.columns[2]]
+    data1 = np.array(data1).reshape(-1, 1)
     # # selecting open column and reshaping
-    # data2 = train_data[train_data.columns[3]]
-    # data2 = np.array(data2).reshape(-1, 1)
+    data2 = train_data[train_data.columns[3]]
+    data2 = np.array(data2).reshape(-1, 1)
     # # selecting high column and reshaping
-    # data3 = train_data[train_data.columns[4]]
-    # data3 = np.array(data3).reshape(-1, 1)
+    data3 = train_data[train_data.columns[4]]
+    data3 = np.array(data3).reshape(-1, 1)
     # # selecting low column and reshaping
-    # data4 = train_data[train_data.columns[5]]
-    # data4 = np.array(data4).reshape(-1, 1)
+    data4 = train_data[train_data.columns[5]]
+    data4 = np.array(data4).reshape(-1, 1)
     #
     # # Function selecting past 3 days data for each column(volume, open, high, low)
-    # def prepare_dataset(data, data_step=1):
-    #     x_data, y_data = [], []
-    #     for k in range(len(data) - data_step):
-    #         b = data[k:(k + data_step), 0]  # k = 0, 0,1,2   3
-    #         x_data.append(b)
-    #         y_data.append(data[k + data_step, 0])
-    #     return np.array(x_data), np.array(y_data)
-    #
-    # data_step = 3
+    def prepare_dataset(data, data_step=1):
+        x_data, y_data = [], []
+        for k in range(len(data) - data_step):
+           b = data[k:(k + data_step), 0]  # k = 0, 0,1,2   3
+           x_data.append(b)
+           y_data.append(data[k + data_step, 0])
+        return np.array(x_data), np.array(y_data)
+    
+    data_step = 3
     # # Calling prepare_dataset function for Volume
-    # volume, y_train = prepare_dataset(data1, data_step)
+    volume, y_train = prepare_dataset(data1, data_step)
     # # Calling prepare_dataset function for open
-    # opn, label = prepare_dataset(data2, data_step)
+    opn, label = prepare_dataset(data2, data_step)
     # # Calling prepare_dataset function for high
-    # high, y_train = prepare_dataset(data3, data_step)
+    high, y_train = prepare_dataset(data3, data_step)
     # # Calling prepare_dataset function for low
-    # low, y_train = prepare_dataset(data4, data_step)
+    low, y_train = prepare_dataset(data4, data_step)
     #
     # # I have allocated features as volume1, volume2, volume3, open1, open2, open3, high1, high2, high3, low1, low2, low3.
     # # Thus I have got 12 features as input for new dataset.
-    # train = np.concatenate((volume, opn, high, low), axis=1)
-    # train = train[0:]
-    # train_data_store = pd.DataFrame(train, columns=['volume1', 'volume2', 'volume3', 'open1', 'open2', 'open3', 'high1',
+    train = np.concatenate((volume, opn, high, low), axis=1)
+    train = train[0:]
+    train_data_store = pd.DataFrame(train, columns=['volume1', 'volume2', 'volume3', 'open1', 'open2', 'open3', 'high1',
     #                                                 'high2', 'high3', 'low1', 'low2', 'low3'])
     # # Appending dates column
-    # train_data_store.insert(0, 'dates', dates_data[1:1257])
+    train_data_store.insert(0, 'dates', dates_data[1:1257])
     # # open column as the target
-    # target = np.array(data2).reshape(-1, 1)
-    # target = target[0:1256]
+    target = np.array(data2).reshape(-1, 1)
+    target = target[0:1256]
     #
     # # Randomize and splitting the data into train and validation(70% and 30%)
-    # X_train, X_test, Y_train, Y_test = train_test_split(train, target, test_size=0.3, random_state=42)
+    X_train, X_test, Y_train, Y_test = train_test_split(train, target, test_size=0.3, random_state=42)
     #
     # # Exporting Train and Test data into .csv files
     # # Converting X_train and Y_train data into .csv
-    # X_Train = pd.DataFrame(X_train)
+    X_Train = pd.DataFrame(X_train)
     #
-    # Y_Train = pd.DataFrame(Y_train)
+    Y_Train = pd.DataFrame(Y_train)
     #
-    # Train_data_csv = pd.concat([X_Train, Y_Train], axis=1)
+    Train_data_csv = pd.concat([X_Train, Y_Train], axis=1)
     #
-    # Train_data_csv.to_csv(r'data\train_data_RNN.csv', index=False)
+    Train_data_csv.to_csv(r'data\train_data_RNN.csv', index=False)
     #
     # # Converting X_test and Y_test data into .csv
     #
-    # X_Test = pd.DataFrame(X_test)
+    X_Test = pd.DataFrame(X_test)
     #
-    # Y_Test = pd.DataFrame(Y_test)
+    Y_Test = pd.DataFrame(Y_test)
     #
-    # Test_data_csv = pd.concat([X_Test, Y_Test], axis=1)
+    Test_data_csv = pd.concat([X_Test, Y_Test], axis=1)
     #
-    # Test_data_csv.to_csv(r'data\test_data_RNN.csv', index=False)
+    Test_data_csv.to_csv(r'data\test_data_RNN.csv', index=False)
 
     # Loading train_data_RNN.csv file
 
